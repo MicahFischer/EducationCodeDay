21ab470c8c727ff16f4ca29404cdb76b677b9bdd
Unknown <micahefish@gmail.com>
2018-11-10 18:15:42 -0700

    Code Day 2 Update

--- education code day/main.js
+++ education code day/main.js
@@ -1,12 +1,12 @@
 var first = true;
-function firstTime(){
+function setup(){
   if(first == true){
     window.open("first.html");
     first = false;
     return true;
   }
   else{
-    window.open("index.html");
+    window.open("feed.html");
     return False;
   }
 }
@@ -26,4 +26,23 @@ function whocountry(event){
 function who(event){
   teachstu = event.target.value;
   console.log(teachstu);
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
 }
